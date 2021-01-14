637188f1e9a43ca783af63179ebdfa132fbd43a7
= joeyclapton <= joeyclapton42@gmail.com>
2021-01-14 19:50:30 -0300

    feat: adiciona método para adicionar nova despesa

--- src/services/api/api.service.js
+++ src/services/api/api.service.js
@@ -44,4 +44,35 @@ export class Api {
 
     return response
   }
+
+  /**
+   * @private
+   * @description - retorna informações da sidebar
+   * @returns {Object} - informações da sidebar
+   */
+  async addExpense({
+    expenseTypeCode,
+    currencyCode,
+    amountSpent,
+    amountTotal,
+    notes,
+    resourceUrl,
+    cardDate,
+  }) {
+    const api = this.url
+    const data = arguments[0]
+
+    const response = await fetch('/expense/add', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: data,
+    })
+      .then(response => response.json())
+      .catch(err => console.error('Erro: ', err))
+
+    return response
+  }
 }
