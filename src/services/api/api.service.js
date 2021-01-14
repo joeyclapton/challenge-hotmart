d1ab1942e2cb42834bbf44fe2c75f89849cf280e
= joeyclapton <= joeyclapton42@gmail.com>
2021-01-14 19:37:21 -0300

    feat: adiciona método para buscar informações do header

--- src/services/api/api.service.js
+++ src/services/api/api.service.js
@@ -2,4 +2,18 @@ export class Api {
   constructor({ url } = {}) {
     this.url = url
   }
+
+  /**
+   * @private
+   * @description - retorna informações do header
+   * @returns {Object} - informações do header
+   */
+  async _getHeader() {
+    const api = this.url
+    const response = await fetch(`${api}/header`)
+      .then(response => response.json())
+      .catch(err => console.error('Erro:', err))
+
+    return response
+  }
 }
