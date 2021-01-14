7138f58efa270f3244e6861bf7a7388a481a3169
= joeyclapton <= joeyclapton42@gmail.com>
2021-01-14 20:02:56 -0300

    feat: remove configurações iniciais

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>Hello world</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
