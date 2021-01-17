75496c59d23d3257bde4a6e9fd6ddf29b3dd369e
= joeyclapton <= joeyclapton42@gmail.com>
2021-01-17 15:32:18 -0300

    feat: adiciona estrutura do componente timeline

---
+++ src/components/hot-timeline/hot-timeline.component.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hot-timeline-component">
+    <div class="">
+      <p>Tipo</p>
+      <p>{{ cardType }}</p>
+    </div>
+    <div class="">
+      <p>Valor</p>
+    </div>
+    <div class="">
+      <p>Observação</p>
+    </div>
+    <div class="">
+      <p>Status</p>
+      <div>
+        <p>
+          {{ status }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    cardType: String,
+    cardDate: String,
+    notes: String,
+    status: String,
+    name: String,
+    email: String,
+    amountSpent: Number,
+    amountTotal: Number,
+  },
+}
+</script>
+
+<style lang="sass">
+.hot-timeline-component
+    align-items: center
+    background-color: #ffffff
+    border-radius: 6px
+    display: flex
+    justify-content: space-between
+    height: 154px
+    margin: 28px
+    padding: 24px
+</style>
