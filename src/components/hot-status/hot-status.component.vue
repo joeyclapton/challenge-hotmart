09ca1c35ed0d9ff5adb2c3252e6f3963152330f8
= joeyclapton <= joeyclapton42@gmail.com>
2021-01-17 14:25:59 -0300

    feat: adiciona componente de status

---
+++ src/components/hot-status/hot-status.component.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="hot-status-component">
+    <div class="status">
+      <p class="status__legend">Status</p>
+      <p class="status__state">{{ status }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    status: String,
+  },
+}
+</script>
+
+<style lang="sass">
+.hot-status-component
+
+    .status
+        align-items: center
+        background-color: #edfff5
+        border: solid 1px #009d43
+        border-radius: 6px
+        display: flex
+        flex-direction: column
+        justify-content: center
+        height: 85px
+        padding: 24px
+        width: 100%
+
+        &__legend
+            color: #006e2f
+            font-size: 16px
+            margin: 0
+
+        &__state
+            color: #009d43
+            font-size: 24px
+            font-weight: bold
+            margin: 0
+</style>
