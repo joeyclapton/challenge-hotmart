510ef6375e5b3b2ccbc57d7822e5452efe534867
= joeyclapton <= joeyclapton42@gmail.com>
2021-01-19 08:11:32 -0300

    feat: adiciona estilo no componente

--- src/components/hot-card-extract/hot-card-extract.component.vue
+++ src/components/hot-card-extract/hot-card-extract.component.vue
@@ -1,23 +1,34 @@
 <template>
   <div class="hot-card-extract-component">
     <header class="header">
-      <p>Saldo</p>
-      <p>
+      <p class="header__title">Saldo</p>
+      <p class="header__value">
         <span>ARS 0,00</span>
       </p>
     </header>
+    <div class="divider"></div>
     <div class="cash">
       <div class="cash__wrapper">
-        <p class="cash__wrapper-label">Gastou</p>
-        <p class="cash__wrapper-value">ARS 0,00</p>
+        <div class="cash__icon">
+          <i class="fas fa-arrow-up"></i>
+        </div>
+        <div>
+          <p class="cash__wrapper-label">Gastou</p>
+          <p class="cash__wrapper-value">ARS 0,00</p>
+        </div>
       </div>
       <div class="cash__wrapper">
-        <p class="cash__wrapper-label">Recebeu</p>
-        <p class="cash__wrapper-value">ARS 0,00</p>
+        <div class="cash__icon">
+          <i class="fas fa-arrow-down"></i>
+        </div>
+        <div>
+          <p class="cash__wrapper-label">Recebeu</p>
+          <p class="cash__wrapper-value">ARS 0,00</p>
+        </div>
       </div>
-      <p>Extrato</p>
     </div>
-    <table>
+    <table class="table">
+      <p class="table__title">Extrato</p>
       <thead>
         <tr>
           <td>Descrição</td>
@@ -30,16 +41,20 @@
           <th>ARS 123.213,21</th>
         </tr>
         <tr class="">
+          <td>Despesas declaradas pelo trooper</td>
+          <td></td>
+        </tr>
+        <tr class="">
           <th>Despesas aprovadas</th>
           <th>ARS 0,00</th>
         </tr>
         <tr class="">
-          <td>Despesas declaradas pelo financeiro</td>
+          <td>Despesas aprovadas pelo financeiro</td>
           <td></td>
         </tr>
         <tr class="">
-          <td>Pagamento realizado</td>
-          <td>ARS 0,00</td>
+          <th>Pagamento realizado</th>
+          <th class="table--highlight">ARS 0,00</th>
         </tr>
         <tr class="">
           <td>Pagamento realizado pelo financeiro</td>
@@ -59,4 +74,71 @@ export default {}
     border: solid 1px #f0f3f7
     border-radius: 6px
     padding: 20px
+    margin-top: 28px
+
+    .divider
+      background-color: #f0f3f7
+      height: 2px
+      margin: 24px auto
+      width: 80%
+
+    .cash
+      display: flex
+      justify-content: center
+
+      &__icon
+        border: 1px solid #d0d3d6
+        border-radius: 50%
+        margin-right: 12px
+        padding: 2px 8px
+
+        .fas
+          color: #d0d3d6
+          font-size: 12px
+
+      &__wrapper
+        align-items: center
+        display: flex
+        justify-content: center
+        margin: 0 6px
+
+    .header
+      &__title
+        color: #6b7480
+        font-size: 14px
+        text-transform: uppercase
+        text-align: center
+
+      &__value
+        color: #053d4e
+        font-size: 24px
+        font-weight: bold
+        text-align: center
+
+    .table
+      &__title
+        border-bottom: 1px solid #f0f3f7
+        margin-bottom: 24px
+        margin-top: 48px
+        padding-bottom: 12px
+
+      th, td
+        text-align: left
+
+      thead
+        td
+          color: #053d4e
+
+      tbody
+        th
+          color: #053d4e
+          font-size: 14px
+          padding: 8px 8px 0 0
+
+          &.table--highlight
+            color: #4acc82
+
+        td
+          color: #9ea4ac
+          font-size: 12px
 </style>
